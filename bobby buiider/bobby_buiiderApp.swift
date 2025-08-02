ef04b1bb36235629bf73408f803816a8e9bb2971
adhavan950 <adhavan_senthil_kumar@students.edu.sg>
2025-08-02 09:54:29 +0800

    Initial Commit

---
+++ bobby buiider/bobby_buiiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobby_buiiderApp.swift
+//  bobby buiider
+//
+//  Created by Adhavan senthil kumar on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bobby_buiiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
