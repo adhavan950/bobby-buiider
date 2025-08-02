33215f9c50d00fb71df6bc80e9a0198f495c4fb2
Adam <adam_zafir_mohammed_sudhir+github@s2024.ssts.edu.sg>
2025-08-02 10:20:30 +0800

    refactored

---
+++ bobby buiider/EmojiView.swift
@@ -0,0 +1,11 @@
+struct EmojiView: View {
+    let symbol: String
+    let size: CGFloat
+    let x: CGFloat
+    let y: CGFloat
+    var body: some View {
+        Text(symbol)
+            .font(.system(size: size))
+            .offset(x: x, y: y)
+    }
+}
